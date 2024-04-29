d21b34b2479debe246aaa21479afd723023d96da
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-04-29 20:56:16 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -3,7 +3,18 @@
 using namespace std;
 
 int main() {
-
+    int number;
+    int sum = 0;
+    while (true) {
+        cout << "Enter number(Entering zero will end the program): ";
+        cin >> number;
+        if (number == 0) {
+            break;
+        }
+        sum += number;
+    }
+    cout << sum << " ";
+    cout << endl << endl;
     system("pause");
     return 0;
 }
