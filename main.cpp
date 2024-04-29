03b9604234c8518b9419af29d7954cd4ca193d8a
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-04-29 20:47:41 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -3,7 +3,22 @@
 using namespace std;
 
 int main() {
+    int number_start, number_end, i;
+    cout << "Enter number start: ";
+    cin >> number_start;
+    cout << "Enter number end: ";
+    cin >> number_end;
+    int start = min(number_start, number_end);
+    int end = max(number_start, number_end);
 
+    i = start;
+    int sum = 0;
+    while (i <= end) {
+        sum += i;
+        i++;
+    }
+    cout << sum << " ";
+    cout << endl << endl;
     system("pause");
     return 0;
 }
