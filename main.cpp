e3fcbb2e3c33b974bc0e00c868aed6a6013aa53f
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-04-29 20:40:16 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -3,7 +3,44 @@
 using namespace std;
 
 int main() {
+    int number_start, number_end, i;
+    cout << "Enter number start: ";
+    cin >> number_start;
+    cout << "Enter number end: ";
+    cin >> number_end;
+    int start = min(number_start, number_end);
+    int end = max(number_start, number_end);
 
+    i = start;
+    while (i <= end) {
+        cout << i << " ";
+        i++;
+    }
+    cout << endl << endl;
+    i = start;
+    while (i <= end) {
+        if (i % 2 == 0) {
+            cout << i << " ";
+        }
+        i++;
+    }
+    cout << endl << endl;
+    i = start;
+    while (i <= end) {
+        if (i % 2 != 0) {
+            cout << i << " ";
+        }
+        i++;
+    }
+    cout << endl << endl;
+    i = start;
+    while (i <= end) {
+        if (i % 7 == 0) {
+            cout << i << " ";
+        }
+        i++;
+    }
+    cout << endl << endl;
     system("pause");
     return 0;
 }
